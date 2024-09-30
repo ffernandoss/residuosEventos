6b2e30c05f0637ba207807a3141ba5c64d9551b4
ffernandoss <fsantama@myuax.com>
2024-09-30 18:42:40 +0200

    tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,10 @@ android {
 
 dependencies {
 
+
+        androidTestImplementation ("androidx.compose.ui:ui-test-junit4:<version>")
+        debugImplementation ("androidx.compose.ui:ui-test-manifest:<version>")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
